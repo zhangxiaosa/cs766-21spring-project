c39c6e9fa104dfcb84d504d48b3d25c1f1edd6c2
zhangxiaosa <15927119728@163.com>
2021-03-02 11:01:16 -0600

    init original cnn

---
+++ networks/origin_cnn.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+# https://www.tensorflow.org/tutorials/images/cnn
+
+class OriginCNN(object):
+  """docstring for OriginCNN"""
+  def __init__(self):
+    self.optimizer = 'adam'
+    # will add more properties
+
+  def cifar10():
+    # cifar10 is used for debugging
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+    # Normalize pixel values to be between 0 and 1
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+
+  def build():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+
+    model.summary()
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+    self.model = model
+
+
+  def train():
+    history = model.fit(train_images, train_labels, epochs=10, 
+                      validation_data=(test_images, test_labels))
+
+
+  def evaluate():
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+    print(test_acc)
+
+
