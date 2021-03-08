6deb8131f8dc50f41486b5ec50f18cab3679fc91
zhangxiaosa <15927119728@163.com>
2021-03-07 22:43:33 -0600

    can train on cifar10

--- networks/models.py
+++ networks/models.py
@@ -8,16 +8,18 @@ class OriginCNN(object):
   """docstring for OriginCNN"""
   def __init__(self):
     self.optimizer = 'adam'
+    self.loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     # will add more properties
 
-  def cifar10():
+  def obtain_cifar10(self):
     # cifar10 is used for debugging
-    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+    (train_images, self.train_labels), (test_images, self.test_labels) = datasets.cifar10.load_data()
 
     # Normalize pixel values to be between 0 and 1
-    train_images, test_images = train_images / 255.0, test_images / 255.0
+    self.train_images, self.test_images = train_images / 255.0, test_images / 255.0
 
-  def build():
+
+  def build(self):
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -30,27 +32,25 @@ class OriginCNN(object):
     model.add(layers.Dense(10))
 
     model.summary()
-    model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    model.compile(optimizer=self.optimizer,
+                loss=self.loss,
                 metrics=['accuracy'])
     self.model = model
 
 
-  def train():
-    history = model.fit(train_images, train_labels, epochs=10, 
-                      validation_data=(test_images, test_labels))
+  def train(self, train_images, train_labels, test_images, test_labels, epochs):
+    self.history = self.model.fit(train_images, train_labels, epochs=epochs, 
+        validation_data=(test_images, test_labels))
 
 
-  def evaluate():
-    plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+  def evaluate(self, test_images, test_labels):
+    plt.plot(self.history.history['accuracy'], label='accuracy')
+    plt.plot(self.history.history['val_accuracy'], label = 'val_accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.ylim([0.5, 1])
     plt.legend(loc='lower right')
 
-    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+    test_loss, test_acc = self.model.evaluate(test_images, test_labels, verbose=2)
 
     print(test_acc)
-
-
--- networks/run.py
+++ networks/run.py
@@ -1,3 +1,12 @@
-import tensorflow as tf
+import models
+import data
 
+myModel = models.OriginCNN()
 
+myData = data.Cifar10()
+
+myModel.build()
+
+myModel.train(myData.train_images, myData.train_labels, myData.test_images, myData.test_labels, 1)
+
+myModel.evaluate(myData.test_images, myData.test_labels)
